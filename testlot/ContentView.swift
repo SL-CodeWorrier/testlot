807f36f14bd5464518c4b61b7caf5223cf41d9e7
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-14 12:29:36 +0530

    Initial Commit

---
+++ testlot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testlot
+//
+//  Created by Chathura Aththanayaka on 2024-03-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testlot/testlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  testlotApp.swift
+//  testlot
+//
+//  Created by Chathura Aththanayaka on 2024-03-14.
+//
+
+import SwiftUI
+
+@main
+struct testlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
